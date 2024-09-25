0da4f5fac9605723dacddde3f3a7a2593b7698a8
vraicheva7 <v.raicheva2020@pmg-pernik.com>
2024-09-25 09:35:27 +0300

    upload project

---
+++ CatShow/CatShow/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CatShow.Data.Entities;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatShow.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Cat> Cats { get; set; }
+    }
+}
---
+++ CatShow/CatShow/Data/Entities/Cat.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CatShow.Data.Entities
+{
+    public class Cat
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MaxLength(30)]
+        public string Breed { get; set; } = null!;
+
+        [Required]
+        [Range(0,15)]
+        public int Age { get; set; }
+
+        public string Picture { get; set; } = null!;
+    }
+}
